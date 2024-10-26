68efced133a688304e9df4ed66fae763165495e9
ogike <tasi.zoltan07@gmail.com>
2024-10-26 23:36:55 +0200

    Playground UI and 2DColliders

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -32,7 +32,6 @@ namespace Dialogue
         [SerializeField] private GameObject playerDialoguePanel;
         [SerializeField] private GameObject playerDialogueChoicesIcon;
         [SerializeField] private TextMeshProUGUI playerDialogueText;
-        [SerializeField] private TextMeshProUGUI choicesText;
 
         
         private Story currentStory;
