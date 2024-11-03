f06e372803cd1200559a227d1be1024ec4d4979c
ogike <tasi.zoltan07@gmail.com>
2024-11-03 18:39:22 +0100

    Menus implemented

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,37 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject creditsPanel;
-    public GameObject controlsPanel;
 
-    public void Start()
-    {
-        creditsPanel.SetActive(false);
-        controlsPanel.SetActive(false);
-    }
-
-    public void ToggleCredits()
-    {
-        bool newValue = !creditsPanel.activeInHierarchy;
-        creditsPanel.SetActive(newValue);
-
-        if (newValue == true)
-            controlsPanel.SetActive(false);
-    }
-    
-    public void ToggleControls()
-    {
-        bool newValue = !controlsPanel.activeInHierarchy;
-        controlsPanel.SetActive(newValue);
-        
-        if (newValue == true)
-            creditsPanel.SetActive(false);
-    }
-    
     public void StartGame()
     {
         //1 has to be the main gameplay loop
         SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
     
     
